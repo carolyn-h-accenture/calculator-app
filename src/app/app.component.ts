a6637c8ce9a381199e6d3d83b66dbebef6f3c2fd
Hilpisch <carolyn.hilpisch@accenture.com>
2022-04-19 09:04:47 -0500

    adding in functions to do math equations and clear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,4 +23,48 @@ export class AppComponent {
       this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
     }
   }
+
+  //doCalculation method that defines the operand to be used
+  private doCalculation(op , secondOp){
+    switch (op){
+      case '+':
+      return this.firstOperand += secondOp; 
+      case '-': 
+      return this.firstOperand -= secondOp; 
+      case '*': 
+      return this.firstOperand *= secondOp; 
+      case '/': 
+      return this.firstOperand /= secondOp; 
+      case '=':
+      return secondOp;
+    }
+  }
+
+  //define the method that will put it all together
+  public getOperation(op: string){
+    console.log(op);
+
+    if(this.firstOperand === null){
+      this.firstOperand = Number(this.currentNumber);
+
+    }else if(this.operator){
+      const result = this.doCalculation(this.operator , Number(this.currentNumber))
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    console.log(this.firstOperand);
+
+  }
+//clear method for the c button
+
+public clear(){
+  this.currentNumber = '0';
+  this.firstOperand = null;
+  this.operator = null;
+  this.waitForSecondNumber = false;
+}
+
 }
