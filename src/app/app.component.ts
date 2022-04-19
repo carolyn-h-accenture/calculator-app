dd6b88d36dc8bb15e605729050e6dd9cf2e5736a
Hilpisch <carolyn.hilpisch@accenture.com>
2022-04-19 08:52:26 -0500

    adding a method to get the current number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator-app';
+
+  currentNumber = '0';
+  firstOperand = null;
+  operator = null;
+  waitForSecondNumber = false;
+
+  //getNumber equation to get the current number
+  public getNumber(v:string) {
+    console.log(v);
+    if(this.waitForSecondNumber) {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    } else {
+      this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
+    }
+  }
 }
